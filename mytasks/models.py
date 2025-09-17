f05f00776264dcbf1432a683bcfc99e0d094070b
Ryan B <barczakryan@outlook.com>
2025-09-17 16:56:52 -0400

    Created demo task, displays task and values for the task.

--- mytasks/models.py
+++ mytasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    task_details = models.TextField()
+    task_date = models.DateTimeField()
+    task_is_complete = models.BooleanField(default=False)
+    task_priority = models.IntegerField(default=0)
+    task_location = models.CharField(max_length=100)
+    task_set_reminder = models.BooleanField(default=False)
--- mytasks/views.py
+++ mytasks/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Task
 
 # Create your views here.
 
 def tasks(request):
-    return HttpResponse("Hello, world. You're at the tasks page.")
+    Tasks = Task.objects.all().values()
+    template = loader.get_template('taskpage.html')
+    context = {
+        'tasks': Tasks,
+    }
+    return HttpResponse(template.render(context, request))
