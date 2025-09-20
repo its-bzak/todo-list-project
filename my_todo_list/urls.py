aada039fb4ec707ffebac0b3c1ded9a0e441b646
Ryan B <barczakryan@outlook.com>
2025-09-19 23:23:13 -0400

    Changes made to CSS, still work to be done

--- my_todo_list/urls.py
+++ my_todo_list/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tasks/', include('mytasks.urls')),
+    path('', include('mytasks.urls')),
     path('admin/', admin.site.urls),
 ]
